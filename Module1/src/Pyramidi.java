578d35db5fc73e406f1438926d371271bb3a8ec7
Juha Tauriainen <juha@bin.fi>
2025-08-18 10:52:29 +0300

    Add module 1, tasks 1 & 2

---
+++ Module1/src/Pyramidi.java
@@ -0,0 +1,14 @@
+public class Pyramidi {
+    public static void main(String[] args) {
+        System.out.println("    *\n" +
+                "   ***\n" +
+                "  *****\n" +
+                " *******"
+        );
+
+        System.out.println("   *");
+        System.out.println("  ***");
+        System.out.println(" *****");
+        System.out.println("*******");
+    }
+}
